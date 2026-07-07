53b0dec4aa725018eb656278d99334d21b5dd514
Nick Krichevsky <nicholas.krichevsky@discordapp.com>
2026-07-07 13:20:11 +0000

    Add ex_doc dependency for publication

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule Manifold.Mixfile do
 
   defp deps do
     [
-      {:benchfella, "~> 0.3.0", only: [:dev, :test], runtime: false}
+      {:benchfella, "~> 0.3.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
     ]
   end
 
